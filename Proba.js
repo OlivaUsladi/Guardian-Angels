b2344c7a76f3dc25d1b6c725d82bdb56bc50951f
Sasha Lyutaya <130230606+OlivaUsladi@users.noreply.github.com>
2023-06-13 16:23:05 +0300

    Add files via upload

--- Proba.js
+++ Proba.js
@@ -1,6 +1,8 @@
+"use strict";
+
 function check() {
-    var p1 = document.getElementById("p1");
-    var p2 = document.getElementById("p2");
+    let p1 = document.getElementById("p1");
+    let p2 = document.getElementById("p2");
     if (p1.value != p2.value) {
       p2.setCustomValidity("Пароли не совпадают");
       return false; // prevent the form from submitting
@@ -12,20 +14,28 @@ function check() {
 
   function CheckPassword() 
 { 
-  var p1 = document.getElementById("p1").value;  
-  if(p1.length < 8) {  
+  let p1 = document.getElementById("p1");  
+  /*if(p1.value.length < 8) {  
 
+    //Выводит внизу формы, а должен как уведомление в строке (set.CustomValidity), но НЕ РАБОТАЕТ
     document.getElementById("message").innerHTML = "**Password length must be atleast 8 characters";  
 
+    //При return завершается программа? Тогда объединить if, если ЭТА МРАЗЬ ЗАРАБОТАЕТ
     return false;  
 
-  }    
-  var pasw = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$/;
-  if(p1.match(pasw)) { 
-  return true;
+  }    */
+  let pasw = /(?=^.{8,}$)((?=.*\d)|(?=.*\W+))(?![.\n])(?=.*[A-Z])(?=.*[a-z]).*$/;
+  if(p1.value.match(pasw)) { 
+    console.log('Cработало');
+    return true;
+    
   }
   else { 
+    //работает только со 2 клика
     p1.setCustomValidity("Пароль должен содержать как минимум одну цифру, одну заглавную и одну строчную букву и быть длиной от 8 до 20 символов");
+    console.log('Работай, падла');
     return false;
   }
 }
+
+
