cfc9491a36b0279140417a20ab80de487cfb8374
Me <127797343+SombraMail@users.noreply.github.com>
2023-08-23 19:58:06 +0300

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -2,7 +2,7 @@ sudo apt update
 sudo apt install python3-pip	
 sudo pip install pdfkit
 sudo apt-get install wkhtmltopdf
-sed -i '/^sudo/d' test.sh
-sed -i '1,2d' test.sh
+sed -i '/^sudo/d' scan.sh
+sed -i '1,2d' scan.sh
 cd scan/main
 python manage.py runserver
