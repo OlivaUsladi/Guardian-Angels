7566e2f394da124ecdf5f35278647d4728740385
Me <127797343+SombraMail@users.noreply.github.com>
2023-10-03 20:24:29 -0400

    Update scan.sh

--- scan.sh
+++ scan.sh
@@ -2,6 +2,8 @@ sudo apt update
 sudo apt install python3-pip	
 sudo pip install pdfkit
 sudo apt-get install wkhtmltopdf
+sudo pip install pexpect
+sudo pip install pandas
 sed -i '/^sudo/d' scan.sh
 sed -i '1,2d' scan.sh
 cd scan/main
